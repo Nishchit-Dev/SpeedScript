a5e66c83450121a43ce95d630c53efaff48fe919
Nishchit-Dev <nishchitpatel84@gmail.com>
2024-10-24 11:16:54 -0500

    added DB

---
+++ src/lib/db.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import mongoose, { Mongoose } from 'mongoose'
+
+interface MongooseConn {
+    conn: Mongoose | null
+    promise: Promise<Mongoose> | null
+}
+
+let cached: MongooseConn = (global as any).mongoose
+
+if (!cached) {
+    cached = (global as any).mongoose = {
+        conn: null,
+        promise: null,
+    }
+}
+
+export const connect = async () => {
+    if (cached.conn) return cached.conn
+
+    cached.promise =
+        cached.promise ||
+        mongoose.connect(process.env.MONGO_URI!, {
+            dbName: 'SpeedScript',
+            bufferCommands: false,
+            connectTimeoutMS: 30000,
+        })
+
+    cached.conn = await cached.promise
+    return cached.conn
+}

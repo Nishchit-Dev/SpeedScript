6a2af4e97b9e08aa7fdfd179324a6582e94db0eb
Nishchit-Dev <nishchitpatel84@gmail.com>
2024-10-24 13:48:33 -0500

    fixed deployment error + fixed conneciton error + fixed insert issue

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
             firstName: first_name,
             lastName: last_name,
             username: username!,
-            photo: image_url,
+            Photo: image_url,
         }
 
         console.log(user)
--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -3,11 +3,10 @@
 import User from '@/lib/models/user.models'
 import { connect } from '@/lib/db'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function createUser(user: any) {
     try {
-        await connect()
-
+        const res = await connect()
+        console.log(res)
         const newUser = await User.create(user)
         return JSON.parse(JSON.stringify(newUser))
     } catch (err) {

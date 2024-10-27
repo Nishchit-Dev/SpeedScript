efa5be9f3046075f0c34b1c7b1f5dbde7963bf5d
Nishchit-Dev <nishchitpatel84@gmail.com>
2024-10-27 15:39:07 -0500

    added hooks for Timer

---
+++ src/app/hooks/useTimer.tsx
@@ -0,0 +1,26 @@
+import { useState, useCallback } from 'react'
+
+const useTimer = () => {
+    const [timerCount, setTimerCount] = useState(0)
+    const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null)
+
+    const startTimer = useCallback(() => {
+        if (intervalId === null) {
+            const id = setInterval(() => {
+                setTimerCount((prevCount) => prevCount + 1)
+            }, 1000)
+            setIntervalId(id)
+        }
+    }, [intervalId])
+
+    const stopTimer = useCallback(() => {
+        if (intervalId !== null) {
+            clearInterval(intervalId)
+            setIntervalId(null)
+        }
+    }, [intervalId])
+
+    return { timer: timerCount, startTimer, stopTimer }
+}
+
+export default useTimer

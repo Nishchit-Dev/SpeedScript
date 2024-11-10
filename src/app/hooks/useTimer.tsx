fd62d209677dc504cfc77aa6e9f5be5955bdc75b
Nishchit-Dev <nishchitpatel84@gmail.com>
2024-11-10 14:46:00 -0600

    added useTimer

--- src/app/hooks/useTimer.tsx
+++ src/app/hooks/useTimer.tsx
@@ -1,9 +1,10 @@
 import { useState, useCallback } from 'react'
 
+
 const useTimer = () => {
     const [timerCount, setTimerCount] = useState(0)
     const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null)
-
+   
     const startTimer = useCallback(() => {
         if (intervalId === null) {
             const id = setInterval(() => {

e2a1e9fd8399f2e9410732f8a108cd80a5d408bf
Nishchit-Dev <nishchitpatel84@gmail.com>
2024-11-21 05:31:53 -0600

    created useLeaderBoard and with fn leaderboard,dailyLeaderboard.getrank,userrank

--- src/app/hooks/leaderBoard/useLeaderBoard.tsx
+++ src/app/hooks/leaderBoard/useLeaderBoard.tsx
@@ -1,14 +1,38 @@
-import { getLeaderboard } from '@/lib/actions/score.actions'
+import {
+    getDailyLeaderboard,
+    getLeaderboard,
+    getUserRank,
+} from '@/lib/actions/score.actions'
 import { useEffect, useState } from 'react'
 
-export const useLeaderboard = () => {
-    const [leaderboard, setLeaderboard] = useState([])
+interface UserRank {
+    _id: string
+    clerkId: string
+    __v: number
+    dailyHighestWpm: number
+    highestWpm: number
+    rank: number
+}
 
+export const useLeaderboard = () => {
+    const [leaderboard, setLeaderboard] = useState<UserRank[]>([])
+    const [dailyLeaderboard, setDailyLeaderboard] = useState<UserRank[]>([])
+    const [userRank, setUserRank] = useState<UserRank | undefined>(undefined)
+
+    const getRank = async (userId?: string) => {
+        await getUserRank(userId).then((res) => {
+            console.log(res)
+            setUserRank(res)
+        })
+    }
     useEffect(() => {
         getLeaderboard().then((res) => {
             setLeaderboard(res)
         })
+        getDailyLeaderboard().then((res) => {
+            setDailyLeaderboard(res)
+        })
     }, [])
 
-    return { leaderboard }
+    return { leaderboard, dailyLeaderboard, getRank, userRank }
 }

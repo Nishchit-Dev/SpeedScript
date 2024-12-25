8e4bfd6859105c8ed6d24c639366176b77b5b06e
Nishchit-Dev <nishchitpatel84@gmail.com>
2024-12-25 19:35:26 +0530

    integrating Room and socket connection.

---
+++ src/app/hooks/websockethooks/useSockets.tsx
@@ -0,0 +1,123 @@
+import { useCallback, useEffect, useState } from 'react'
+
+const GameState = {
+    CONNECTING: 'connecting',
+    WAITING: 'waiting',
+    COUNTDOWN: 'countdown',
+    IN_PROGRESS: 'in_progress',
+    FINISHED: 'finished',
+    GAME_RESET: 'game_reset',
+}
+
+const useSocket = ({ cursor }: { cursor: any }) => {
+    function generateRandomCode() {
+        // Generate 4 random letters
+        const letters = Array.from(
+            { length: 4 },
+            () => String.fromCharCode(65 + Math.floor(Math.random() * 26)) // A-Z
+        ).join('')
+
+        // Generate 2 random digits
+        const digits = Math.floor(10 + Math.random() * 90).toString() // 10-99
+
+        return letters + digits
+    }
+    const [roomData, setRoomData] = useState({
+        username: '',
+        roomID: 'its_me',
+        roomInfo: {},
+        roomText: '',
+    })
+    const [text, setText] = useState('')
+    const [playerControls, setPlayerControls] = useState(false)
+    const [gameState, setGameState] = useState(GameState.CONNECTING)
+    const [countDown, setCountDown] = useState(0)
+    const [playerProgress, setPlayerProgress] = useState({
+        x: 0,
+        y: 0,
+    })
+    const [connection, setConnection] = useState<WebSocket>()
+
+    const connect = useCallback(() => {
+        let user = generateRandomCode()
+        setRoomData((prev) => {
+            return { ...prev, username: user }
+        })
+
+        const URL = 'ws://localhost:8080/ws/room'
+        const ws = new WebSocket(
+            `${URL}?username=${encodeURIComponent(
+                user
+            )}&room_id=${encodeURIComponent(roomData.roomID)}`
+        )
+
+        ws.onopen = () => {
+            console.log('socket connected')
+            setConnection(ws)
+        }
+
+        ws.onclose = (event) => {
+            console.log('socket closed ', event.code)
+            setGameState(GameState.CONNECTING)
+        }
+
+        ws.onerror = (error) => {
+            console.log('socket error ', error)
+        }
+
+        ws.onmessage = (event) => {
+            try {
+                const message = JSON.parse(event.data)
+
+                switch (message.type) {
+                    case 'room_state':
+                        console.log('room_state', message.data)
+                        setRoomData((prev) => {
+                            return {
+                                ...prev,
+                                roomText:
+                                    message.data.text ||
+                                    'no data is passed in this variable so user will need to type this shit out of nowhere i hope you understand this',
+                            }
+                        })
+                        setGameState(message.data.status)
+                        setRoomData((prev) => {
+                            return { ...prev, roomInfo: message.data.players }
+                        })
+                        break
+                    case 'countdown':
+                        console.log(message.data)
+
+                        setCountDown(message.data)
+                        break
+                }
+            } catch (err) {
+                console.log('err ', err)
+            }
+        }
+
+        return () => {
+            if (ws && ws.readyState === WebSocket.OPEN) {
+                ws.close()
+            }
+        }
+    }, [roomData])
+
+    useEffect(() => {
+        connect()
+    }, [])
+    const toggleReady = () => {
+        setPlayerControls(!playerControls)
+        if (connection && connection.readyState == WebSocket.OPEN) {
+            connection.send(
+                JSON.stringify({
+                    type: 'ready',
+                    data: !playerControls,
+                })
+            )
+        }
+    }
+    return { roomData, toggleReady, playerControls, countDown, gameState }
+}
+
+export default useSocket

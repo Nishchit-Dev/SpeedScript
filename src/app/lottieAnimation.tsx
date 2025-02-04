b21f77ec744016c5eb2393bd654c73d46f64b13f
Nishchit-Dev <nishchitpatel84@gmail.com>
2025-02-03 21:35:23 -0600

    added animiation component

---
+++ src/app/lottieAnimation.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+
+const FireAnimation = () => {
+    return (
+        <DotLottieReact src="/animation/fire.lottie" loop autoplay className='w-[20px] h-[20px]' />
+    )
+}
+
+export default FireAnimation

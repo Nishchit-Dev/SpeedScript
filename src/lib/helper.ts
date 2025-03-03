9097b58eb47239e29885237d3cbdfc9aace8b042
Nishchit-Dev <nishchitpatel84@gmail.com>
2025-03-02 21:19:12 -0600

    fixing build error + adding getWebSocketURL

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,3 +1,5 @@
+import { create } from 'domain'
+
 export const calculateTimeDifferences = (timestamps: any[] = []) => {
     if (timestamps.length < 2) {
         return []
@@ -34,5 +36,69 @@ export const getUserHisotry = () => {
         return parseData
     } else {
         return { error: 'no user history' }
+    }
+}
+interface WebSocketUrl {
+    ws: string;
+    http?: string;
+    https?: string;
+    routes: {
+        wss: {
+            joinRoom: string;
+            adminRole: string;
+            createRoom: string;
+        };
+
+        https: {
+            joinRoom: string;
+            adminRole: string;
+            createRoom: string;
+        };
+    };
+}
+
+export const getWebSocketUrl = (): WebSocketUrl => {
+    let envUrl = process.env.NEXT_PUBLIC_WEBSOCKET_URL
+    if (!envUrl) {
+        envUrl = 'localhost:8080'
+        console.error(
+            'WEBSOCKET_URL is not defined in the environment variables'
+        )
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+        return {
+            ws: `wss://${envUrl}`,
+            https: `https://${envUrl}`,
+            routes: {
+                wss: {
+                    joinRoom: `wss://${envUrl}/ws/room`,
+                    adminRole: `wss://${envUrl}/api/admin/change-role`,
+                    createRoom: `wss://${envUrl}/api/create-room`,
+                },
+                https: {
+                    joinRoom: `https://${envUrl}/ws/room`,
+                    adminRole: `https://${envUrl}/api/admin/change-role`,
+                    createRoom: `https://${envUrl}/api/create-room`,
+                },
+            },
+        }
+    } else {
+        return {
+            ws: `ws://${envUrl}/ws/room`,
+            http: `http://${envUrl}`,
+            routes: {
+                wss: {
+                    joinRoom: `ws://${envUrl}/ws/room`,
+                    adminRole: `ws://${envUrl}/api/admin/change-role`,
+                    createRoom: `ws://${envUrl}/api/create-room`,
+                },
+                https: {
+                    joinRoom: `http://${envUrl}/ws/room`,
+                    adminRole: `http://${envUrl}/api/admin/change-role`,
+                    createRoom: `http://${envUrl}/api/create-room`,
+                },
+            },
+        }
     }
 }

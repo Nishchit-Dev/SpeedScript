50f761cb4a3c429472c4730b8913ab58895f7e8b
Nishchit-Dev <nishchitpatel84@gmail.com>
2025-03-24 21:37:17 -0600

    added profile page

--- src/app/profile/[[...rest]]/page.tsx
+++ src/app/profile/[[...rest]]/page.tsx
@@ -1,9 +1,37 @@
-import { UserProfile } from '@clerk/nextjs'
+'use client'
+import { UserProfile, useUser } from '@clerk/nextjs'
+import Heatmap from './Heatmap.tsx/HeatMap'
+import { useEffect, useState } from 'react'
+import { populateData } from '@/lib/actions/user.actions'
+import { set } from 'mongoose'
+import Image from 'next/image'
+import ProfileComponent from './profile/profile'
 
 export default function Profile() {
+    const { user } = useUser()
+
+    const [flag, setFlag] = useState(false)
+    useEffect(() => {
+        if (user?.id) {
+            setFlag(true)
+        } else {
+            setFlag(false)
+        }
+    }, [user])
     return (
-        <div className="flex flex-1 justify-center items-center h-screen">
-            <UserProfile routing="hash" />
+        <div className="flex flex-1 justify-center items-start h-screen font-jetBrainsMono ">
+            {/* <UserProfile routing="hash" /> */}
+            <div className="flex flex-col gap-10">
+                <div className="flex flex-row gap-10">
+                    <ProfileComponent clerkId={user?.id || ''}>
+                        {flag ? (
+                            <Heatmap clerkId={user?.id || ''} />
+                        ) : (
+                            <p>Loading</p>
+                        )}
+                    </ProfileComponent>
+                </div>
+            </div>
         </div>
     )
 }

0a2b6277c5f96084e1a80a4983b11bc03a7c39cc
Nishchit-Dev <nishchitpatel84@gmail.com>
2025-03-30 02:13:09 -0600

    added getProfileBages compoent

--- src/components/BadgeComponent.ts
+++ src/components/BadgeComponent.ts
@@ -23,3 +23,16 @@ export function getBadgeImageForLeaderBoard(wpm: number): string {
     if (wpm >= 110) return 'Badge_09.svg' // Elite 110
     return 'Badge_01.svg' // Fallback image if WPM is invalid
 }
+
+export const getProfileBadges = (wpm: number) => {
+    if (wpm >= 0 && wpm < 10) return 'BadgeFrame-0.svg' // Starter
+    if (wpm >= 10 && wpm < 20) return 'BadgeFrame-1.svg' // Learner
+    if (wpm >= 20 && wpm < 30) return 'BadgeFrame-2.svg' // Rookie
+    if (wpm >= 30 && wpm < 40) return 'BadgeFrame-3.svg' // Improver
+    if (wpm >= 40 && wpm < 60) return 'BadgeFrame-4.svg' // Proficient
+    if (wpm >= 60 && wpm < 70) return 'BadgeFrame-5.svg' // Skilled
+    if (wpm >= 70 && wpm < 90) return 'BadgeFrame-6.svg' // Expert
+    if (wpm >= 90 && wpm < 110) return 'BadgeFrame-7.svg' // Ace
+    if (wpm >= 110) return 'BadgeFrame-8.svg' // Elite
+    return 'Badge_01.svg' // Fallback image if WPM is invalid
+}
